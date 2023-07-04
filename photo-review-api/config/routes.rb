5238c3b57470191ca69da667d1c9bd2e85f7806b
Oanh Nguyen <myspamvn@yahoo.com>
2023-07-04 23:54:27 +0900

    build: add devise to authenticate

--- photo-review-api/config/routes.rb
+++ photo-review-api/config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
-  # get 'albums', to: 'albums#index'
-  # get 'albums/new', to: 'albums#new', as: :new_album
-  # post 'albums', to: 'albums#create'
-  # get 'albums/show'
-  # get 'albums/edit'
-  # get 'albums/update'
-  # get 'albums/destroy'
-  # resources :albums, only: %i[index show new create edit update]
-  resources :albums
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  root 'albums#index'
 end
--- photo-review-api/db/schema.rb
+++ photo-review-api/db/schema.rb
@@ -10,42 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_01_143913) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_04_143649) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'albums', force: :cascade do |t|
-    t.string 'name'
-    t.date 'expiry_date'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "albums", force: :cascade do |t|
+    t.string "name"
+    t.date "expiry_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'photo_user_reviews', force: :cascade do |t|
-    t.bigint 'photo_id', null: false
-    t.bigint 'review_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['photo_id'], name: 'index_photo_user_reviews_on_photo_id'
-    t.index ['review_id'], name: 'index_photo_user_reviews_on_review_id'
+  create_table "photo_user_reviews", force: :cascade do |t|
+    t.bigint "photo_id", null: false
+    t.bigint "review_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["photo_id"], name: "index_photo_user_reviews_on_photo_id"
+    t.index ["review_id"], name: "index_photo_user_reviews_on_review_id"
   end
 
-  create_table 'photos', force: :cascade do |t|
-    t.decimal 'size'
-    t.string 'dimension'
-    t.bigint 'album_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['album_id'], name: 'index_photos_on_album_id'
+  create_table "photos", force: :cascade do |t|
+    t.decimal "size"
+    t.string "dimension"
+    t.bigint "album_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["album_id"], name: "index_photos_on_album_id"
   end
 
-  create_table 'reviews', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'photo_user_reviews', 'photos'
-  add_foreign_key 'photo_user_reviews', 'reviews'
-  add_foreign_key 'photos', 'albums'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "photo_user_reviews", "photos"
+  add_foreign_key "photo_user_reviews", "reviews"
+  add_foreign_key "photos", "albums"
 end
