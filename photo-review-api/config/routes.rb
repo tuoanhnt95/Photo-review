bb11605b9cdc6c50f76fe05903532b01fe0951ca
Oanh Nguyen <myspamvn@yahoo.com>
2023-08-29 01:49:13 +0900

    feat: backend receives image arrays from frontend

--- photo-review-api/config/routes.rb
+++ photo-review-api/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :photo_user_reviews
-  resources :photos
   resources :reviews
-  resources :albums
+  resources :albums do
+    resources :photos
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
