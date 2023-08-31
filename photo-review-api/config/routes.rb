8904eed3462cf2ea1d0db018c851871b62445825
Oanh Nguyen <myspamvn@yahoo.com>
2023-08-31 23:57:25 +0900

    feat: create photo-review component with photo

--- photo-review-api/config/routes.rb
+++ photo-review-api/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :photo_user_reviews
   resources :reviews
   resources :albums do
-    resources :photos
+    resources :photos, only: %i[index create]
   end
+  resources :photos, only: %i[show update destroy]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
