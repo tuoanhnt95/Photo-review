83407ad8f860862f7a003c7fa28449f8b987cf71
Oanh Nguyen <myspamvn@yahoo.com>
2023-09-07 00:05:08 +0900

    feat: add review result to photo object sent to frontend

--- photo-review-api/app/controllers/photos_controller.rb
+++ photo-review-api/app/controllers/photos_controller.rb
@@ -6,8 +6,14 @@ class PhotosController < ApplicationController
   # GET /albums/:album_id/photos
   def index
     @photos = Photo.where(album_id: params[:album_id])
-
-    render json: @photos
+    # return photo objects with review results
+    result = []
+    @photos.each do |photo|
+      photo_object = {}.merge(photo.attributes)
+      photo_object[:review_results] = get_review_result(photo)
+      result.push(photo_object)
+    end
+    render json: result
   end
 
   # GET /photos/:id
@@ -41,6 +47,39 @@ class PhotosController < ApplicationController
   end
 
   private
+
+  def get_review_result(photo)
+    user = User.first
+    reviews = photo.photo_user_reviews
+    review_by_user = reviews.find { |review| review.user_id == user.id }
+    # Only show result after current user has reviewed
+    return nil if reviews.empty? || review_by_user.nil?
+
+    # 0: No, 1: Yes, 2: Maybe
+    # If all reviews are No, then No
+    # If all reviews are Yes, then Yes
+    # Otherwise, then Maybe
+    values = map_review_value(reviews)
+    if reviews_all_yes?(values)
+      1
+    elsif reviews_all_no?(values)
+      0
+    else
+      2
+    end
+  end
+
+  def map_review_value(reviews)
+    reviews.map { |review| Review.find(review.review_id).value }
+  end
+
+  def reviews_all_yes?(values)
+    values.all? { |value| value == 1 }
+  end
+
+  def reviews_all_no?(values)
+    values.all?(&:zero?)
+  end
 
   def save_photos_to_db(processed_images)
     result = []
