c3bde9afcb8a78576ac23ec24828636c96bde35e
Oanh Nguyen <myspamvn@yahoo.com>
2023-11-12 17:50:21 +0900

    feat: delete multiple photos

--- photo-review-client/src/views/Album.vue
+++ photo-review-client/src/views/Album.vue
@@ -1,84 +1,121 @@
 <template>
   <div class="absolute top-0 left-0 w-full h-full">
-    <div class="relative w-full" :class="{ 'opacity-10 saturate-0' : (isUploadingPhoto || isShowingPhoto)}">
+    <div class="relative w-full"
+      :class="{ 'opacity-10 saturate-0': isUploadingPhoto || isShowingPhoto }"
+    >
       <div class="flex justify-between mx-8 mt-4">
         <div v-if="!isEditing" class="label-text">
           {{ album.name }}
         </div>
         <div v-else class="label-text">
-          <input type="text" v-model="albumName" class="pl-2 rounded text-black" >
+          <input type="text"
+            v-model="albumName"
+            class="pl-2 rounded text-black"
+          />
         </div>
 
         <div v-if="!isEditing" class="flex">
           <font-awesome-icon icon="fa-solid fa-pen"
             class="self-center text-slate-400"
-            @click="startEditingAlbum"
+            @click.prevent="startEditingAlbum"
           />
         </div>
         <div v-else class="flex space-x-6 self-center">
           <font-awesome-icon icon="fa-solid fa-floppy-disk"
-            @click="saveEditAlbum()"
+            @click.prevent="saveEditAlbum()"
           />
           <font-awesome-icon icon="fa-solid fa-x"
-            @click="cancelEditAlbum()"
+            @click.prevent="cancelEditAlbum()"
           />
         </div>
       </div>
-  <!-- Filter -->
-      <div class="flex justify-between w-100 ml-3">
-        <div class="flex items-center">
-          <font-awesome-icon icon="fa-solid fa-calendar-days" class="mr-2 self-center"/>
-          <div v-if="!isEditing" class="text-slate-400">
-            {{ formatDate(album.expiry_date) }}
+      <!-- Filter -->
+      <div class="w-100 h-8 ml-3 mt-3">
+        <div v-if="selectedPhotoIds.length === 0"
+          class="flex justify-between items-center h-full"
+        >
+          <div class="flex items-center">
+            <font-awesome-icon icon="fa-solid fa-calendar-days"
+              class="mr-2 self-center"
+            />
+            <div v-if="!isEditing" class="text-slate-400">
+              {{ formatDate(album.expiry_date) }}
+            </div>
+            <div v-else>
+              <input type="date" class="text-black" v-model="albumExpiryDate" />
+            </div>
           </div>
-          <div v-else>
-            <input type="date" class="text-black" v-model="albumExpiryDate">
-          </div>
-        </div>
-        <div class="flex w-42 bg-slate-200 border divide-x divide-y-0 divide-slate-400 text-slate-600 rounded-sm">
-          <div v-for="opt in filterReview" :key="opt.icon">
-            <div class="btn-filter w-14 px-2 py-0.5"
-              :class="{ 'btn-filter-selected': opt.selected }"
-              @click="opt.selected = !opt.selected"
-            >
-              <div>
+          <div class="flex w-42 bg-slate-200 border divide-x divide-slate-400 text-slate-600 rounded-sm">
+            <div v-for="opt in filterReview" :key="opt.icon">
+              <div class="btn-filter w-14 px-2 py-0.5"
+                :class="{ 'btn-filter-selected': opt.selected }"
+                @click="opt.selected = !opt.selected"
+              >
                 <font-awesome-icon :icon="`fa-solid fa-${opt.icon}`" />
+                <div>({{ numberOfPhotosWithReview(opt.value) }})</div>
               </div>
-              <div>({{ numberOfPhotosWithReview(opt.value) }})</div>
             </div>
           </div>
         </div>
-      </div>
-
-  <!-- Photos -->
-      <div class="grid grid-cols-5 gap-0.5 w-full mt-4">
-        <div @click="isUploadingPhoto = true" class="photo-container flex">
-          <font-awesome-icon icon="fa-solid fa-plus" class="m-auto text-violet-600"/>
+        <div v-else class="flex justify-between items-center h-full mr-3">
+          <div class="flex justify-between items-center h-full w-20">
+            <font-awesome-icon icon="fa-solid fa-xmark"
+              class="p-2 cursor-pointer"
+              @click.prevent="cancelSelectPhotos"
+            />
+            <div>{{ selectedPhotoIds.length }}</div>
+          </div>
+          <div class="flex justify-between w-20">
+            <font-awesome-icon icon="fa-solid fa-share-from-square" />
+            <font-awesome-icon icon="fa-regular fa-trash-can"
+              class="mr-2"
+              @click.prevent="deletePhotos"
+            />
+          </div>
         </div>
-        <div v-for="(photo, i) in photos" :key="i" class="relative cursor-pointer">
-          <div class="photo-container flex justify-center cursor-pointer" @click="showPhoto(photo.id)">
-            <AdvancedImage :cldImg="getCloudinaryImage(photo.image, photo.angle)"
-              :id="photo.image" class="object-cover"
+      </div>
+      <!-- Photos -->
+      <div class="grid grid-cols-5 gap-0.5 w-full mt-4">
+        <div class="photo-container flex"
+          @click.prevent="isUploadingPhoto = true"
+        >
+          <font-awesome-icon
+            icon="fa-solid fa-plus"
+            class="m-auto text-violet-600"
+          />
+        </div>
+        <div v-for="(photo, i) in photos" :key="i"
+          class="relative cursor-pointer"
+        >
+          <div class="photo-container flex justify-center cursor-pointer"
+            @click.prevent="showPhoto(photo.id)"
+          >
+            <AdvancedImage :id="photo.image"
+              :cldImg="getCloudinaryImage(photo.image, photo.angle)"
+              class="object-cover"
               :class="getPhotoClass(photo)"
             />
           </div>
           <!-- </RouterLink> -->
-          <font-awesome-icon icon="fa-solid fa-x"
-            class="absolute top-1 right-1 z-50 text-slate-400"
-            @click="deletePhoto(photo.id)"
+          <font-awesome-icon icon="fa-solid fa-circle-check"
+            class="icon-circle-check"
+            :class="{ 'icon-circle-check-selected': selectedPhotoIds.includes(photo.id) }"
+            @click.prevent="toggleSelectPhoto(photo.id)"
           />
         </div>
       </div>
     </div>
 
-    <PhotoUpload v-if="isUploadingPhoto"
+    <PhotoUpload
+      v-if="isUploadingPhoto"
       :album="album"
       class="absolute w-full z-10"
       @uploaded-new-photo="(photos) => addPhoto(photos)"
       @close-upload-photo="isUploadingPhoto = false"
     />
 
-    <Photo v-if="isShowingPhoto && photoShowing"
+    <Photo
+      v-if="isShowingPhoto && photoShowing"
       :photo="photoShowing"
       :photos="photos"
       class="absolute top-0 left-0 w-full z-50"
@@ -104,30 +141,30 @@ import PhotoUpload from '../components/PhotoUpload.vue';
 const route = useRoute();
 
 interface Album {
-  id: number,
-  name: string,
-  last_upload_batch: number,
-  expiry_date: Date
+  id: number;
+  name: string;
+  last_upload_batch: number;
+  expiry_date: Date;
 }
 
 interface Photo {
-  id: number,
-  name: string,
-  image: string,
-  angle: number,
-  album_id: number,
-  review_results: number | null,
-  created_at: Date,
-  updated_at: Date
+  id: number;
+  name: string;
+  image: string;
+  angle: number;
+  album_id: number;
+  review_results: number | null;
+  created_at: Date;
+  updated_at: Date;
 }
 
 const album = ref<Album>({
   id: -1,
   name: '',
   last_upload_batch: 0,
-  expiry_date: new Date()
+  expiry_date: new Date(),
 });
-onBeforeMount(async() => {
+onBeforeMount(async () => {
   await axios
     .get(`http://localhost:3000/albums/${albumId.value}`)
     .then((response) => {
@@ -144,11 +181,11 @@ onBeforeMount(async() => {
 const cld = new Cloudinary({
   cloud: {
     cloudName: 'djnvimner',
-  }
+  },
 });
 const getCloudinaryImage = (publicId: String, angle: number) => {
   return cld.image(`photo_review/${publicId}`).rotate(byAngle(angle));
-}
+};
 
 const albumId = computed(() => {
   const id = route.params.id;
@@ -164,63 +201,94 @@ const startEditingAlbum = () => {
   isEditing.value = true;
   albumName.value = album.value.name;
   albumExpiryDate.value = album.value.expiry_date;
-}
+};
 
 const cancelEditAlbum = () => {
   albumName.value = '';
   albumExpiryDate.value = new Date();
   isEditing.value = false;
-}
+};
 
-const saveEditAlbum = async() => {
+const saveEditAlbum = async () => {
   await axios
     .patch('http://localhost:3000/albums/' + albumId.value, {
       name: albumName.value,
-      expiry_date: albumExpiryDate.value
+      expiry_date: albumExpiryDate.value,
     })
     .then(() => {
       album.value.name = albumName.value;
       album.value.expiry_date = albumExpiryDate.value;
       cancelEditAlbum();
-    }).catch((error) => {
-      console.log(error);
     })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// Select photos for option
+const selectedPhotoIds = ref<number[]>([]);
+function toggleSelectPhoto(photoId: number) {
+  if (selectedPhotoIds.value.includes(photoId)) {
+    const index = selectedPhotoIds.value.findIndex((x) => (x = photoId));
+    selectedPhotoIds.value.splice(index, 1);
+  } else {
+    selectedPhotoIds.value.push(photoId);
+  }
 }
 
-const deletePhoto = async (photoId: number) => {
-  if (confirm('Delete photo?') === false) {
+function cancelSelectPhotos() {
+  selectedPhotoIds.value = [];
+}
+
+const deletePhotos = async () => {
+  let message = ''
+  if (selectedPhotoIds.value.length === 1) {
+    const selectedPhoto = photos.value.find((x: Photo) => x.id === selectedPhotoIds.value[0]);
+    message = `Delete '${ selectedPhoto?.name }'?`;
+  } else {
+    message = `Delete ${ selectedPhotoIds.value.length } photos?`
+  }
+  if (confirm(message) === false) {
     return;
   }
   await axios
-    .delete('http://localhost:3000/photos/' + photoId)
-    .then(() => {
-      removePhoto(photoId);
-    }).catch((error) => {
-      console.log(error);
+    .delete('http://localhost:3000/delete_photos', {
+      data: {
+        photo_ids: selectedPhotoIds.value,
+      }
+    }).then(() => {
+      selectedPhotoIds.value.forEach((photoId) => {
+        removePhoto(photoId);
+      });
     })
-}
+    .catch((error) => {
+      console.log(error);
+    });
+};
 const removePhoto = (photoId: number) => {
-  const index = photosData.value.findIndex((photo: Photo) => photo.id === photoId);
+  const index = photosData.value.findIndex(
+    (photo: Photo) => photo.id === photoId
+  );
   photosData.value.splice(index, 1);
-}
+};
 const addPhoto = (photos: [Photo]) => {
-  photos.forEach(photo => {
+  photos.forEach((photo) => {
     // photosData.value.unshift(photo);
     photosData.value.push(photo);
   });
-}
+};
 
 const formatDate = (date: Date) => {
   date = new Date(date);
   return date.toISOString().split('T')[0];
-}
+};
 
-const photos = computed (() => {
-  return photosData.value
+const photos = computed(() => {
+  return photosData.value;
 });
 
 const photosData = ref([<Photo>{}]);
-onBeforeMount(async() => {
+onBeforeMount(async () => {
   await axios
     .get(`http://localhost:3000/albums/${albumId.value}/photos`)
     .then((response) => {
@@ -241,7 +309,7 @@ const filterReview = ref([
   { selected: false, value: 1, icon: 'check' },
   { selected: false, value: 2, icon: 'question' },
   { selected: false, value: 0, icon: 'xmark' },
-  { selected: false, value: null, icon: 'exclamation' }
+  { selected: false, value: null, icon: 'exclamation' },
 ]); // Yes Maybe No
 const filterIsSelected = computed(() => {
   return filterReview.value.some((x) => x.selected);
@@ -258,11 +326,11 @@ const photoShowing = ref<Photo>();
 const showPhoto = (photoId: Number) => {
   photoShowing.value = photosData.value.find((x) => x.id === photoId);
   isShowingPhoto.value = true;
-}
+};
 const updatePhoto = (photo: Photo) => {
   const index = photosData.value.findIndex((x) => x.id === photo.id);
   photosData.value[index] = photo;
-}
+};
 function closeReviewPhoto() {
   isShowingPhoto.value = false;
   // photoShowing.value = {};
@@ -271,13 +339,15 @@ function closeReviewPhoto() {
 // style
 function getPhotoClass(photo: Photo) {
   let result = '';
-  const filterOfPhoto = filterReview.value.find((x) => x.value === photo.review_results);
+  const filterOfPhoto = filterReview.value.find(
+    (x) => x.value === photo.review_results
+  );
   if (!filterIsSelected.value) {
     return result;
   } else if (!filterOfPhoto || !filterOfPhoto.selected) {
-    result += ' opacity-10 saturate-0'
+    result += ' opacity-10 saturate-0';
   }
-  return result
+  return result;
 }
 </script>
 
@@ -296,5 +366,25 @@ function getPhotoClass(photo: Photo) {
 .btn-filter-selected {
   background-color: white;
   color: var(--color-primary);
+}
+
+.icon-circle-check {
+  position: absolute;
+  top: 0.25rem;
+  left: 0.25rem;
+  z-index: 50;
+  opacity: 0;
+}
+
+.icon-circle-check:hover {
+  opacity: 1;
+  color: var(--slate-300);
+}
+
+.icon-circle-check-selected, .icon-circle-check-selected:hover  {
+  opacity: 1;
+  color: var(--color-primary);
+  background-color: white;
+  border-radius: 50%;
 }
 </style>
